93cebbe6489f6acf061c9f354e97a60d4546803c
Asfandyar <a_asfandyar1@hotmail.com>
2023-09-18 22:36:35 +0100

    First commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
+                
+                Section {
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                }
+            }
         }
-        .padding()
     }
 }
 
