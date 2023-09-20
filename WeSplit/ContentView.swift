99b3d2b20c9e3525e9560c33846e8c103d9e3687
Asfandyar <a_asfandyar1@hotmail.com>
2023-09-20 05:06:38 +0100

    Added extra features and completed project

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,19 +11,75 @@ struct ContentView: View {
     @State private var checkAmout = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmout / 100 * tipSelection
+        let grandTotal = checkAmout + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmout, format:
-                        .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                        .keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmout, format:
+                            .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Group {
+                    Section {
+                        Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(tipPercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                        }
+                        
+                        TextField("Tip Percentage", value: $tipPercentage, format: .percent)
+                            .keyboardType(.decimalPad)
+                            .focused($amountIsFocused)
+                        
+                    } header: {
+                        Text("How much tip do you wnat to leave?")
+                    }
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Amount per person")
+                }
+                
+                Section {
+                    Text((totalPerPerson * Double(numberOfPeople + 2)), format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Total for all")
+                }
             }
-            
-            Section {
-                Text(checkAmout, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
             }
         }
     }
@@ -35,5 +91,12 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
+
+
 
 
